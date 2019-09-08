f63d43b7eec26f2d1d43b99651306fff9ff44399
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-09-08 22:42:01 +0530

    index: Add comments and doc strings.

--- src/build_index.py
+++ src/build_index.py
@@ -9,6 +9,13 @@ from utils import *
 class Index:
 
 
+    """
+
+    This class acts as the data-structure for the inverted index.
+    Provides functionality to update, search, load and dump the the index.
+
+    """
+
     def __init__(self, db_name):
         
         self.db_name = db_name
@@ -16,6 +23,15 @@ class Index:
         self.load_index()
 
     def update_index(self, filename, token_freq):
+
+        """
+
+        this function updates the index with every new file insertion.
+
+        param filename: name of the file to be inserted in the index
+        param token_freq: dict with frequency ratio of words in the given file
+
+        """
 
         for token, freq in token_freq.items():
 
@@ -47,6 +63,18 @@ class Index:
 
     def get_compound_result(self, keyword_list, num_of_result = 5):
 
+        """
+
+        This function accepts a list of keywords,
+        and returns the intersection of the resultset for each of the keyword.
+
+        @param keyword_list: list of keywords to be searched
+        @param num_of_result: maximum number of results needed (default: 5)
+
+        @return: list of result
+
+        """
+
         ps = nltk.PorterStemmer()
         result = self.get_result(ps.stem(keyword_list[0]), num_of_result)
 
@@ -56,6 +84,13 @@ class Index:
         return result
 
     def load_index(self):
+
+        """
+
+        loads the inverted index from the binary file for persistant storage.
+
+        """
+
 
         db_path = pathlib.Path(INDEX_PATH + self.db_name)
 
@@ -67,6 +102,11 @@ class Index:
             print ("Index Load Successful.")
 
     def dump_index(self):
+
+        """
+        dumps the inverted index to a binary file for persistant storage.
+        """
+
 
         with open(INDEX_PATH + self.db_name, 'wb') as file:
             pickle.dump(self.__inverted_index, file)
