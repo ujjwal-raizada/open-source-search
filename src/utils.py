9627a179d1d0672a5785d8df2114d7503c22190c
PranjalGupta2199 <pranjalgupta2199@gmail.com>
2019-09-22 04:36:20 +0530

    scraper: Fetches Readme data and adds to the corpus

--- src/utils.py
+++ src/utils.py
@@ -2,5 +2,5 @@ CORPUS = '../corpus'
 INDEX_PATH = '../indexes/'
 PARSED_DOC_FILE = 'read_doc'
 sites = [
-    #"https://github.com/search?p={}&q=stars%3A%3E0&s=stars&type=Repositories",] # \
+    "https://github.com/search?p={}&q=stars%3A%3E0&s=stars&type=Repositories",  \
     "https://github.com/search?o=desc&p={}&q=stars%3A%3E1&s=forks&type=Repositories"]
